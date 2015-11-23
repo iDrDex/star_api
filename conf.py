66e4dbb11357b386299f31bda84c64aaa2ae8247
idrdex <idrdex@gmail.com>
2015-11-23 11:15:25 -0800

    Make directories automatically

--- conf.py
+++ conf.py
@@ -1,3 +1,7 @@
+import os
+
 SERIES_MATRIX_URL = 'ftp://ftp.ncbi.nih.gov/pub/geo/DATA/SeriesMatrix/'
 SERIES_MATRIX_MIRROR = "geo_mirror/DATA/SeriesMatrix/"
+os.makedirs(SERIES_MATRIX_MIRROR, exist_ok=True)
 CSV_CACHE = "csv"
+os.makedirs(CSV_CACHE, exist_ok=True)
