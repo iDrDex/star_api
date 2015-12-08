0825fcbdd5e227b85a5c32f5903c63ab66384fd1
Daniel Himmelstein <daniel.himmelstein@gmail.com>
2015-12-07 17:22:55 -0800

    Allow specification of cache location
    For the transition to a standalone package: idrdex/star_api#8

--- starapi/conf.py
+++ starapi/conf.py
@@ -2,8 +2,19 @@ import os
 
 SERIES_MATRIX_URL = 'ftp://ftp.ncbi.nih.gov/pub/geo/DATA/SeriesMatrix/'
 
-SERIES_MATRIX_MIRROR = "geo_mirror/DATA/SeriesMatrix/"
-if not os.path.exists(SERIES_MATRIX_MIRROR): os.makedirs(SERIES_MATRIX_MIRROR)
+def configure(directory):
+    """Configure starapi to create cache locations in the specified directory"""
 
-CSV_CACHE = "csv"
-if not os.path.exists(CSV_CACHE): os.makedirs(CSV_CACHE)
+    global SERIES_MATRIX_MIRROR
+    global CSV_CACHE
+
+    SERIES_MATRIX_MIRROR = os.path.join(directory, 'geo_mirror/DATA/SeriesMatrix/')
+    CSV_CACHE = os.path.join(directory, 'csv/')
+
+    if not os.path.exists(SERIES_MATRIX_MIRROR):
+        print('creating SERIES_MATRIX_MIRROR directory')
+        os.makedirs(SERIES_MATRIX_MIRROR)
+
+    if not os.path.exists(CSV_CACHE):
+        print('creating CSV_CACHE directory')
+        os.makedirs(CSV_CACHE)
